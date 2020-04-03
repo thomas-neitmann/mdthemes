6d3ac36fbed198a5747cb95b672d432433fada5d
thomas-neitmann <th.neitmann@gmail.com>
2020-04-03 11:58:33 +0200

    Set text to 'plain' by default

--- R/as_md_theme.R
+++ R/as_md_theme.R
@@ -1,10 +1,16 @@
 #' @importFrom ggtext element_markdown
-as_md_theme <- function(theme) {
+as_md_theme <- function(theme, all_plain = TRUE) {
+  args <- list()
+  if (all_plain) {
+    args$face <- "plain"
+  }
   theme +
     ggplot2::theme(
-      plot.title = element_markdown(),
-      plot.subtitle = element_markdown(),
-      plot.caption = element_markdown(),
-      axis.title = element_markdown()
+      plot.title = do.call(element_markdown, args),
+      plot.subtitle = do.call(element_markdown, args),
+      plot.caption = do.call(element_markdown, args),
+      axis.title = do.call(element_markdown, args),
+      axis.title.x = do.call(element_markdown, args),
+      axis.title.y = do.call(element_markdown, args)
     )
 }

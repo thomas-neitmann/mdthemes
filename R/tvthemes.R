1f184e97db2a98127bafb565e5c554b2721b9049
thomas-neitmann <th.neitmann@gmail.com>
2020-04-10 21:45:20 +0200

    Add `tvthemes` (close #1)

---
+++ R/tvthemes.R
@@ -0,0 +1,80 @@
+#' ggthemes Markdown Themes
+#'
+#' ggthemes themes supporting markdown syntax
+#'
+#' @param ... Arguments passed on to the corresponding \code{ggplot2} theme
+#'
+#' @return An object of class \code{theme} that can be added to any \code{ggplot}
+#'
+#' @rdname tvthemes
+#' @export
+md_theme_avatar <- function(...) {
+  as_md_theme(tvthemes::theme_avatar(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_brooklyn99 <- function(...) {
+  as_md_theme(tvthemes::theme_brooklyn99(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_hildaDay <- function(...) {
+  as_md_theme(tvthemes::theme_hildaDay(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_hildaDusk <- function(...) {
+  as_md_theme(tvthemes::theme_hildaDusk(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_hildaNight <- function(...) {
+  as_md_theme(tvthemes::theme_hildaNight(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_parksAndRec <- function(...) {
+  as_md_theme(tvthemes::theme_parksAndRec(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_parksAndRec_light <- function(...) {
+  as_md_theme(tvthemes::theme_parksAndRec_light(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_parksAndRecLight <- function(...) {
+  as_md_theme(tvthemes::theme_parksAndRecLight(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_rickAndMorty <- function(...) {
+  as_md_theme(tvthemes::theme_rickAndMorty(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_simpsons <- function(...) {
+  as_md_theme(tvthemes::theme_simpsons(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_spongeBob <- function(...) {
+  as_md_theme(tvthemes::theme_spongeBob(...))
+}
+
+#' @rdname tvthemes
+#' @export
+md_theme_theLastAirbender <- function(...) {
+  as_md_theme(tvthemes::theme_theLastAirbender(...))
+}
+

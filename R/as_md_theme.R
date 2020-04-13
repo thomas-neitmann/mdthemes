b2484ebbf2a79136a0acdfa16e5c33bf9b0eba2d
thomas-neitmann <th.neitmann@gmail.com>
2020-04-13 12:29:53 +0200

    Simplify `as_md_theme()` code

--- R/as_md_theme.R
+++ R/as_md_theme.R
@@ -1,16 +1,16 @@
-#' @importFrom ggtext element_markdown
 as_md_theme <- function(theme, all_plain = TRUE) {
-  args <- list()
   if (all_plain) {
-    args$face <- "plain"
+    emd <- ggtext::element_markdown(face = "plain")
+  } else {
+    emd <- ggtext::element_markdown()
   }
   theme +
     ggplot2::theme(
-      plot.title = do.call(element_markdown, args),
-      plot.subtitle = do.call(element_markdown, args),
-      plot.caption = do.call(element_markdown, args),
-      axis.title = do.call(element_markdown, args),
-      axis.title.x = do.call(element_markdown, args),
-      axis.title.y = do.call(element_markdown, args)
+      plot.title = emd,
+      plot.subtitle = emd,
+      plot.caption = emd,
+      axis.title = emd,
+      axis.title.x = emd,
+      axis.title.y = emd
     )
 }

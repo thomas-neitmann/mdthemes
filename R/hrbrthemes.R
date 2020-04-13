0e41d2a587a26cd09c43642edc18675f67973afe
thomas-neitmann <th.neitmann@gmail.com>
2020-04-13 12:28:11 +0200

    Add examples

--- R/hrbrthemes.R
+++ R/hrbrthemes.R
@@ -6,6 +6,24 @@
 #'
 #' @return An object of class \code{theme} that can be added to any \code{ggplot}
 #'
+#' @examples
+#' library(ggplot2)
+#' data(mtcars)
+#'
+#' p <- ggplot(mtcars, aes(hp, mpg)) +
+#'   geom_point() +
+#'   labs(
+#'     title = "A **bold** title",
+#'     subtitle = "An *italics* subtitle",
+#'     caption = "A <span style = 'color:blue'>blue</span> caption"
+#'   )
+#'
+#' # With a 'usual' theme the text is not rendered
+#' p
+#'
+#' # By adding an 'md' theme the text is rendered properly
+#' p + md_theme_ipsum()
+#'
 #' @rdname hrbrthemes
 #' @export
 md_theme_ft_rc <- function(...) {

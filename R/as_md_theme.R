3e2d9f52a8a77318dd4663ae453ed7da4daf6c4b
thomas-neitmann <th.neitmann@gmail.com>
2020-04-13 13:48:22 +0200

    Add documentation and export `as_md_theme()`

--- R/as_md_theme.R
+++ R/as_md_theme.R
@@ -1,3 +1,31 @@
+#' Turn a usual theme into a markdown theme
+#'
+#' Transform any \code{ggplot2} theme into a theme that renders text as markdown
+#'
+#' @param theme An object of class \code{theme}
+#' @param all_plain \code{logical}. Should all text be plain by default?
+#'
+#' @examples
+#' library(ggplot2)
+#' data(mtcars)
+#'
+#' # Create a custom theme
+#' my_theme <- theme_gray() +
+#'   theme(
+#'     panel.grid.minor = element_blank(),
+#'     panel.grid.major = element_blank()
+#'   )
+#' p <- ggplot(mtcars, aes(hp)) +
+#'   geom_histogram() +
+#'   ggtitle("Mixing **bold** and *italics* is easy")
+#'
+#' # Text is not rendered
+#' p + my_theme
+#'
+#' # Text is rendered properly
+#' p + as_md_theme(my_theme)
+#'
+#' @export
 as_md_theme <- function(theme, all_plain = TRUE) {
   if (all_plain) {
     emd <- ggtext::element_markdown(face = "plain")

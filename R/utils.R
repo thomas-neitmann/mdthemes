cc80a19450f6e0abe6f45ca0fe735236e120db11
Thomas Neitmann <th.neitmann@gmail.com>
2020-06-19 20:51:28 +0200

    Refactor and expand helper functions to color words

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,34 @@
-md_col <- function(words, color) {
-  paste0("<span style=color:\'", color, "\'>", words, "</span>")
+#' @export
+md_title <- function(label) {
+  md_labs(title = label)
 }
 
-md_title <- function(title) {
-  ggplot2::ggtitle(glue::glue(title))
+#' @export
+md_subtitle <- function(label) {
+  md_labs(subtitle = label)
+}
+
+#' @export
+md_caption <- function(label) {
+  md_labs(caption = label)
+}
+
+#' @importFrom ggplot2 waiver
+#' @export
+md_labs <- function(title = waiver(),
+                    subtitle = waiver(),
+                    caption = waiver(),
+                    tag = waiver()) {
+  glue_if <- function(e) {
+    if (inherits(e, "waiver")) e else glue::glue(e)
+  }
+  col <- function(words, color) {
+    paste0("<span style=color:\'", color, "\'>", words, "</span>")
+  }
+  ggplot2::labs(
+    title = glue_if(title),
+    subtitle = glue_if(subtitle),
+    caption = glue_if(caption),
+    tag = glue_if(tag)
+  )
 }
